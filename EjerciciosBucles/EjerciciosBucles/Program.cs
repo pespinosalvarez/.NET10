3c427d5b0be8f9984a2fa256ca7a662e6959bca9
Pablo Espinosa <paesal01@gmail.com>
2026-06-24 22:41:12 +0200

    Refactor Program.cs with additional exercises

--- EjerciciosBucles/EjerciciosBucles/Program.cs
+++ EjerciciosBucles/EjerciciosBucles/Program.cs
@@ -188,6 +188,19 @@ namespace EjerciciosBucles
             //**
             //***
             //****
+            
+            Console.WriteLine("Da un número entero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
+            {
+                 for (int j = 1; j <= i; j++)
+                 {
+                     Console.Write("*");
+                 }
+             Console.WriteLine();
+            }
+            
         }
 
         static void Ejercicio14()
