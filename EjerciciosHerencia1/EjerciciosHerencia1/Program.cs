1331658c16c2d2a428b417d02c307556813ec4e6
Pablo Espinosa <paesal01@gmail.com>
2026-07-09 18:45:54 +0200

    Add files via upload

---
+++ EjerciciosHerencia1/EjerciciosHerencia1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EjerciciosHerencia1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ EjerciciosHerencia1/EjerciciosHerencia1/Tienda.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjerciciosHerencia1
+{
+    internal class Tienda
+    {
+    }
+}
